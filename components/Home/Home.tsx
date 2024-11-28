3bec33af71aa0069e44e24da674f27dbe96e3749
amnajee-art <aftabamna13@gmail.com>
2024-11-28 10:55:55 +0500

    finalized

---
+++ components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import AllProducts from "./AllProducts";
+import Category from "./Category";
+import Hero from "./Hero";
+
+
+export default function  Home(){
+    return (
+        <div>
+            <Hero/>
+            <Category/>
+            <AllProducts/>
+            
+        </div>
+    )
+}
